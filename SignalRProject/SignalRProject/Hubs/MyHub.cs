aea28baeeb87ce0ab73a2e7c1981790eb4404e34
Erdem Buzluk <buzlukerdem@gmail.com>
2023-01-12 13:07:04 +0300

    First Push
    Created a simple SignalR project and myHub class.

---
+++ SignalRProject/SignalRProject/Hubs/MyHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRProject.Hubs
+{
+	public class MyHub:Hub
+	{
+		public async Task SendMessageAsync(string message)
+		{
+			await Clients.All.SendAsync("receiveMessage", message);
+		}
+	}
+}
