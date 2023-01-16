92be8b7bfd7e6f4581131ee054f58fd99c495475
Erdem Buzluk <buzlukerdem@gmail.com>
2023-01-16 12:04:12 +0300

    userJoin - userLeaved connect
    created userJoined and userLeaved func. and successfuly tested

--- SignalRProject/SignalRProject/Hubs/MyHub.cs
+++ SignalRProject/SignalRProject/Hubs/MyHub.cs
@@ -4,9 +4,24 @@ namespace SignalRProject.Hubs
 {
 	public class MyHub:Hub
 	{
+		static List<string> clients = new List<string>();	
 		public async Task SendMessageAsync(string message)
 		{
 			await Clients.All.SendAsync("receiveMessage", message);
+		}
+		// baglanma durumu
+		public override async Task OnConnectedAsync()
+		{
+			clients.Add(Context.ConnectionId);
+			await Clients.All.SendAsync("clients", clients);
+			await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+		}
+		// baglanti kopma durumu
+		public override async Task OnDisconnectedAsync(Exception? exception)
+		{
+			clients.Remove(Context.ConnectionId);
+			await Clients.All.SendAsync("clients", clients);
+			await Clients.All.SendAsync("userLeaved", Context.ConnectionId);
 		}
 	}
 }
